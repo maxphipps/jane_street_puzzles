e80555c877135fc9791810d403c3de517fd1f359
Max Phipps <maxphipps4@gmail.com>
2023-01-21 16:45:15 +0000

    Update description

--- scripts/puzzle012023.py
+++ scripts/puzzle012023.py
@@ -3,19 +3,24 @@ from numpy.linalg import inv
 
 """
 Puzzle name: 'Lesses More'
+Approach:
+- Cast into linear problem
+- Constrain problem such that one corner is 0 valued (by exploiting that the problem requires minimum sum of values)
+- Recursively solve until 10mn limit reached
 """
 
+# Optimal initial guess
 corners = np.array((3,1,0))
-op = np.array([[1, 0, -1],
+inv_op = inv(np.array([[1, 0, -1],
                [1, -1, -1],
-               [0, 1, -2]])
+               [0, 1, -2]]))
 print(corners)
 print('Begin recursive generation')
 n=0
 for i in range(100):
     n+=1
     corners2 = [_*2. for _ in corners]
-    corners2 = np.dot(inv(op), corners2)
+    corners2 = np.dot(inv_op, corners2)
     # scale to maintain integer
     # common factors
     while np.all([_%2 == 0 for _ in corners2]):
